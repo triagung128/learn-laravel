fcdcbd5c15d6ed1992fc512a1a6a22f40f26b77b
triagung128 <triagung128@gmail.com>
2020-05-09 14:10:01 +0700

    try route console

--- routes/console.php
+++ routes/console.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
+
+Artisan::command('say:hello', function () {
+    $this->info("Hello, I'm studying laravel :D");
+});
