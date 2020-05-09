afd44008000e29b455bd251ea22596346f903d62
triagung128 <triagung128@gmail.com>
2020-05-09 14:45:21 +0700

    try http method in route

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,31 @@ Route::get('/product/display', 'ProductController@showAll');
 Route::get('/product/save', 'ProductController@saveNew');
 
 Route::get("/pintu-masuk", 'TestController@pintuMasuk');
+
+// Method GET untuk melihat daftar list
+Route::get("/products/list", 'ProductController@list');
+
+// Method POST untuk membuat produk baru
+Route::post("/product/create", 'ProductController@create');
+
+// Method PUT untuk mengupdate data produk
+Route::put("/products/{id}", 'ProductController@update');
+
+// Mengizinkan lebih dari 1 HTTP Method : PUT dan PATCH
+Route::match(["PUT", "PATCH"], "/products/{id}", "ProductController@update");
+
+// Mengizinkan semua HTTP Method
+Route::any("/products/{id}", 'ProductController@action');
+
+// Membuat nama untuk route
+Route::get('/products/list', 'ProductController@list')->name('products');
+
+// Membuat kelompok route (Route Group)
+Route::group(["prefix" => "/products"], function(){
+    Route::get('/all', 'ProductController@all');
+    Route::get('/bag', 'ProductController@bag');
+    Route::get('/latest', 'ProductController@latest');
+});
+
+// Membuat route view
+Route::view("/path", "nama.view");
