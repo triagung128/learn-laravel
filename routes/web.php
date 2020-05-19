56ea603ba5f482e5e09bd36fa640eae474bf349b
triagung128 <triagung128@gmail.com>
2020-05-19 14:27:21 +0700

    create route

--- routes/web.php
+++ routes/web.php
@@ -61,3 +61,11 @@ Route::get("show-item-list", "ItemController@showAllItems");
 
 // Membuat Route Resource
 Route::resource("categories", "CategoryController");
+
+Route::group(["prefix" => "latihan"], function(){
+    Route::get("/kategori/all", "CategoryController@index");
+    Route::get("/kategori/search", "CategoryController@search");
+    Route::get("/kategori/{id}/delete", "CategoryController@delete");
+    Route::get("/kategori/{id}/restore", "CategoryController@restore");
+    Route::get("/kategori/{id}/permanent-delete", "CategoryController@permanentDelete");
+});
