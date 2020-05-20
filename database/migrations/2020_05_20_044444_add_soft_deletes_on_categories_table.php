1d9d83fee90ed62029fa3585c3a7e8d0dcee5ffa
triagung128 <triagung128@gmail.com>
2020-05-20 16:51:16 +0700

    update file migration

--- database/migrations/2020_05_20_044444_add_soft_deletes_on_categories_table.php
+++ database/migrations/2020_05_20_044444_add_soft_deletes_on_categories_table.php
@@ -26,7 +26,7 @@ class AddSoftDeletesOnCategoriesTable extends Migration
     public function down()
     {
         Schema::table('categories', function (Blueprint $table) {
-            
+            $table->dropSoftDeletes();
         });
     }
 }
