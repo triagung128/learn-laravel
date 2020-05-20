bcd1f4eb897216893e5577d7445473f19d308205
triagung128 <triagung128@gmail.com>
2020-05-20 17:21:40 +0700

    update file migration

---
+++ database/migrations/2020_05_20_095211_create_handphones_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHandphonesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('handphones', function (Blueprint $table) {
+            $table->id();
+            $table->string('phone_num')->unique();
+            $table->bigInteger('user_id')->unsigned()->unique();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('handphones');
+    }
+}
